998f9363ae72c54f3f698807640988e8413891df
komalbharat <gharmode.komal@gmail.com>
2023-01-20 22:27:45 +0530

    This is Unit Test FOr all

--- src/JUnitForRegistration/Registration.java
+++ src/JUnitForRegistration/Registration.java
@@ -2,16 +2,31 @@ package JUnitForRegistration;
 import java.util.regex.Pattern;
 import java.util.Scanner;
 
-
 public class Registration {
-    static boolean Name(String str1) {
+    static boolean Name(String str1){
         boolean b1 = Pattern.compile("^[A-Z]{1}[A-Za-z]{2,}$").matcher(str1).matches();
         return b1;
     }
-    static boolean LastName(String str2) {
+
+    static boolean Last(String str2){
         boolean b2 = Pattern.compile("^[A-Z]{1}[A-Za-z]{2,}$").matcher(str2).matches();
         return b2;
     }
+
+    static boolean Email(String str3){
+        boolean b3 = Pattern.compile("^[A-Za-z0-9]+[@][a-zA-Z]+[.][a-zA-Z]+[.]?[a-zA-Z]{2}$").matcher(str3).matches();
+        return b3;
+    }
+
+    static boolean Phone(String str4){
+        boolean b4 = Pattern.compile("^(91)[ ]{1}[1-9]{1}[0-9]{9}$").matcher(str4).matches();
+        return b4;
+    }
+    static boolean Password(String str5){
+        boolean b5 = Pattern.compile("^[0-9]{1,}[!@#$%^&]{1,}[A-z]{1,}[a-zA-Z0-9]{5,}$").matcher(str5).matches();
+        return b5;
+    }
+
     public static void main(String[] args) {
     }
 
--- src/JUnitForRegistration/TestReg.java
+++ src/JUnitForRegistration/TestReg.java
@@ -5,10 +5,25 @@ import java.util.regex.Pattern;
 
 public class TestReg extends Registration {
     @Test
-    public void RegTest (){
-        Assert.assertEquals(true, Name("Avi"));
-        Assert.assertEquals(true,LastName("Gharmode"));
+    public void NameTest(){
+        Assert.assertEquals(true, Name("Ankit") );
+    }
 
+    @Test
+    public void LastNameTest(){
+        Assert.assertEquals(true, Last("Kumar") );
+    }
+    @Test
+    public void EmailTest(){
+        Assert.assertEquals(true, Email("Kankit40@gmail.com") );
+    }
+    @Test
+    public void PhoneTest(){
+        Assert.assertEquals(true, Phone("91 9898989898") );
+    }
+    @Test
+    public void PasswordTest(){
+        Assert.assertEquals(true, Password("01@abcdefgh") );
     }
 
 }
